35c7f99cc2462f9506e9fa5a55721af843e76849
is99el <ismaelazaaouaj2004@gmail.com>
2023-11-29 12:40:56 +0100

    DB Ismael

--- components/connect.php
+++ components/connect.php
@@ -3,7 +3,7 @@
 $user="user";
 $pass="12345";
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=voedselbank;port=8888', $user, $pass);
+    $conn = new PDO('mysql:host=localhost;dbname=voedselbank;port=3306', $user, $pass);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
