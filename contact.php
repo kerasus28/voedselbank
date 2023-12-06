e38c05fa6315791dbdb1bdfe44866a8c95a8bcae
Burak <burak.aydin28@hotmail.com>
2023-12-06 10:35:22 +0100

    messages
    messages folder added with code

--- contact.php
+++ contact.php
@@ -66,7 +66,7 @@ if(isset($_POST['send'])){
       <input type="text" name="name" placeholder="enter your name" required maxlength="20" class="box">
       <input type="email" name="email" placeholder="enter your email" required maxlength="50" class="box">
       <input type="number" name="number" min="0" max="9999999999" placeholder="enter your number" required onkeypress="if(this.value.length == 10) return false;" class="box">
-      <textarea name="msg" class="box" placeholder="enter your message" cols="30" rows="10"></textarea>
+      <input name="msg" class="box" placeholder="enter your message" cols="30" rows="10"></textarea>
       <input type="submit" value="send message" name="send" class="btn">
    </form>
 
