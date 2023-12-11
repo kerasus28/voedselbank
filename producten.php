fbcdb72a727b56b72c022cdd5e7f88e9de93e8c7
ismael <ismaelazaaouaj2004@gmail.com>
2023-12-11 13:58:51 +0100

    Buggs gefixt

--- producten.php
+++ producten.php
@@ -86,9 +86,13 @@ if(isset($_GET['delete'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>products</title>
 
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <link rel="stylesheet" href="css/admin_style.css">
+<link rel="stylesheet" href="css/admin_style.css">
+
+<!-- custom css file link  -->
+<link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
--- producten_overzicht.php
+++ producten_overzicht.php
@@ -20,9 +20,15 @@ if(!isset($user_id)){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>products</title>
 
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <link rel="stylesheet" href="css/admin_style.css">
+   
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
