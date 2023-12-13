965a61b19ee1641eeaa51b52607641b8a73a6c19
Munguss <skyroelvink@gmail.com>
2023-12-13 10:48:09 +0100

    About veranderd naar = Familie

--- components/user_header.php
+++ components/user_header.php
@@ -19,7 +19,7 @@
 
       <nav class="navbar">
          <a href="home.php">home</a>
-         <a href="about.php">about</a>
+         <a href="familie.php">Familie</a>
          <a href="orders.php">orders</a>
          <a href="shop.php">shop</a>
          <a href="contact.php">contact</a>
